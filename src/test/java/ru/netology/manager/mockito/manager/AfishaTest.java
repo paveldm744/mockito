35023cdd05cc8d85b475e8ad7a3fb1581537e081
Павел Козодаев <paveldm744@gmail.com>
2022-08-26 11:15:59 +0300

    manager

---
+++ src/test/java/ru/netology/manager/mockito/manager/AfishaTest.java
@@ -0,0 +1,47 @@
+package ru.netology.manager.mockito.manager;
+
+import jdk.jfr.Experimental;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AfishaTest {
+    Afisha afisha = new Afisha();
+
+    @Test
+
+    public void addedMovies() {
+        afisha.addMovie("Movie1");
+        afisha.addMovie("Movie2");
+        afisha.addMovie("Movie3");
+        afisha.addMovie("Movie4");
+        afisha.addMovie("Movie5");
+        afisha.addMovie("Movie6");
+        afisha.addMovie("Movie7");
+        afisha.addMovie("Movie8");
+        afisha.addMovie("Movie9");
+        afisha.addMovie("Movie10");
+
+        String[] expected = {"Movie1", "Movie2", "Movie3", "Movie4", "Movie5", "Movie6", "Movie7", "Movie8", "Movie9", "Movie10"};
+        String[] actual = afisha.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+
+    public void shouldFindLast() {
+        afisha.addMovie("Movie1");
+        afisha.addMovie("Movie2");
+        afisha.addMovie("Movie3");
+        afisha.addMovie("Movie4");
+        afisha.addMovie("Movie5");
+        afisha.addMovie("Movie6");
+        afisha.addMovie("Movie7");
+        afisha.addMovie("Movie8");
+        afisha.addMovie("Movie9");
+        afisha.addMovie("Movie10");
+
+        String[] expected = {"Movie10", "Movie9", "Movie8", "Movie7", "Movie6", "Movie5", "Movie4", "Movie3", "Movie2", "Movie1"};
+        String[] actual = afisha.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
